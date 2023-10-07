1e58814e3c028784bc9820d29297840db707465a
tristripe <lrolfg@gmail.com>
2023-10-07 19:38:35 +0300

    Updates

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-import "./src/scss/main.scss";
-import GlobalContextProvider from "./src/context/GlobalContextProvider";
-const React = require("react");
-
-export const wrapRootElement = ({ element }) => {
-  return <GlobalContextProvider>{element}</GlobalContextProvider>;
-};
--- gatsby-ssr.js
+++
@@ -1,6 +0,0 @@
-const React = require("react");
-const GlobalContextProvider = require("./src/context/GlobalContextProvider").default;
-
-exports.wrapRootElement = ({ element }) => {
-  return <GlobalContextProvider>{element}</GlobalContextProvider>;
-};
